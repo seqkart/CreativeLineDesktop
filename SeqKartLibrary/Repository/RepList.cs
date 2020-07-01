4feca1f9a78499feab0daaf8e046e6b84bf0f17c
Seqkart Solutions <Seqkart Solutions@SEQKARTSOLUTION>
2020-07-01 13:12:34 +0530

    Revert "Local Commit"
    This reverts commit 2b6db00149038f2a1cd900ad04583a7d4e1bee53.

--- SeqKartLibrary/Repository/RepList.cs
+++ SeqKartLibrary/Repository/RepList.cs
@@ -9,112 +9,110 @@ namespace SeqKartLibrary.Repository
 {
     public class RepList<T> where T : class
     {
-
+        
         public SqlConnection con;
         private void connection()
         {
             con = new SqlConnection(ProjectFunctionsUtils.ConnectionString);
         }
-            public List<T> returnListClass(string query, DynamicParameters param)
+        public List<T> returnListClass(string query, DynamicParameters param)
+        {
+            try
             {
-                try
-                {
-                    connection();
-                    con.Open();
-                    IList<T> Tlista = SqlMapper.Query<T>(con, query, param, null, true, null, commandType: CommandType.Text).ToList();
-                    con.Close();
-                    return Tlista.ToList();
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
+                connection();
+                con.Open();
+                IList<T> Tlista = SqlMapper.Query<T>(con, query, param, null, true, null, commandType: CommandType.Text).ToList();
+                con.Close();
+                return Tlista.ToList();
             }
-
-            public List<T> returnListClass_SP(string query, DynamicParameters param)
+            catch (Exception)
             {
-                try
-                {
-                    connection();
-                    con.Open();
-                    IList<T> Tlista = SqlMapper.Query<T>(con, query, param, null, true, null, commandType: CommandType.StoredProcedure).ToList();
-                    con.Close();
-                    return Tlista.ToList();
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
+                throw;
             }
+        }
 
-            public T returnClass_SP(string query, DynamicParameters param)
+        public List<T> returnListClass_SP(string query, DynamicParameters param)
+        {
+            try
             {
-                try
-                {
-                    connection();
-                    con.Open();
-                    //     return this.con.Query( query, param, null, true, null, commandType: CommandType.StoredProcedure).FirstOrDefault();
-                    T Tlista = SqlMapper.Query<T>(con, query, param, null, true, null, commandType: CommandType.StoredProcedure).FirstOrDefault();
-                    con.Close();
-                    return Tlista;
-                }
-                catch (Exception)
-                {
-                    throw;
-
-                }
+                connection();
+                con.Open();
+                IList<T> Tlista = SqlMapper.Query<T>(con, query, param, null, true, null, commandType: CommandType.StoredProcedure).ToList();
+                con.Close();
+                return Tlista.ToList();
             }
-
-            public T returnClass(string query, DynamicParameters param)
+            catch (Exception)
             {
-                try
-                {
-                    connection();
-                    con.Open();
-                    //     return this.con.Query( query, param, null, true, null, commandType: CommandType.StoredProcedure).FirstOrDefault();
-                    T Tlista = SqlMapper.Query<T>(con, query, param, null, true, null, commandType: CommandType.Text).FirstOrDefault();
-                    con.Close();
-                    return Tlista;
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
+                throw;
             }
+        }
 
-            public List<T1> returnListClass_1<T1>(string query, DynamicParameters param)
+        public T returnClass_SP(string query, DynamicParameters param)
+        {
+            try
             {
-                try
-                {
-                    connection();
-                    con.Open();
-                    IList<T1> Tlista = SqlMapper.Query<T1>(con, query, param, null, true, null, commandType: CommandType.Text).ToList();
-                    con.Close();
-                    return Tlista.ToList();
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
+                connection();
+                con.Open();
+                //     return this.con.Query( query, param, null, true, null, commandType: CommandType.StoredProcedure).FirstOrDefault();
+                T Tlista = SqlMapper.Query<T>(con, query, param, null, true, null, commandType: CommandType.StoredProcedure).FirstOrDefault();
+                con.Close();
+                return Tlista;
             }
-
-            public List<T1> returnListClass_SP_1<T1>(string query, DynamicParameters param)
+            catch (Exception)
             {
-                try
-                {
-                    connection();
-                    con.Open();
-                    IList<T1> Tlista = SqlMapper.Query<T1>(con, query, param, null, true, null, commandType: CommandType.StoredProcedure).ToList();
-                    con.Close();
-                    return Tlista.ToList();
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
+                throw;
             }
+        }
+
+        public T returnClass(string query, DynamicParameters param)
+        {
+            try
+            {
+                connection();
+                con.Open();
+                //     return this.con.Query( query, param, null, true, null, commandType: CommandType.StoredProcedure).FirstOrDefault();
+                T Tlista = SqlMapper.Query<T>(con, query, param, null, true, null, commandType: CommandType.Text).FirstOrDefault();
+                con.Close();
+                return Tlista;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<T1> returnListClass_1<T1>(string query, DynamicParameters param)
+        {
+            try
+            {
+                connection();
+                con.Open();
+                IList<T1> Tlista = SqlMapper.Query<T1>(con, query, param, null, true, null, commandType: CommandType.Text).ToList();
+                con.Close();
+                return Tlista.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<T1> returnListClass_SP_1<T1>(string query, DynamicParameters param)
+        {
+            try
+            {
+                connection();
+                con.Open();
+                IList<T1> Tlista = SqlMapper.Query<T1>(con, query, param, null, true, null, commandType: CommandType.StoredProcedure).ToList();
+                con.Close();
+                return Tlista.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
 
 
-        
     }
 }
