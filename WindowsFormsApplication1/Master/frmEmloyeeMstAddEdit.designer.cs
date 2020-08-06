ba78d2e6ddb4da174e58e2e4f4d66c2ac95791fe
Seqkart Solutions <Seqkart Solutions@SEQKARTSOLUTION>
2020-08-06 18:52:49 +0530

    CHANGES MADE

--- WindowsFormsApplication1/Master/frmEmloyeeMstAddEdit.designer.cs
+++ WindowsFormsApplication1/Master/frmEmloyeeMstAddEdit.designer.cs
@@ -1618,7 +1618,7 @@
             // 
             // HelpGrid
             // 
-            this.HelpGrid.Location = new System.Drawing.Point(142, 96);
+            this.HelpGrid.Location = new System.Drawing.Point(142, 30);
             this.HelpGrid.MainView = this.HelpGridView;
             this.HelpGrid.Name = "HelpGrid";
             this.HelpGrid.Size = new System.Drawing.Size(420, 274);
